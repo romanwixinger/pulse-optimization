d0fc305ddec9dc3069c07246d79bc6ce09b4842c
romanwixinger <roman.wixinger@gmail.com>
2023-02-06 18:17:54 +0100

    [main] Added quantum algorithms.
    TODO: Standardize the variable n_qubits to the convention that Qiskit uses.

--- src/quantum_algorithms.py
+++ src/quantum_algorithms.py
@@ -1,16 +1,9 @@
-import scipy.special
-
-import numpy as np
-
 from qiskit import QuantumCircuit
-from qiskit.circuit import Parameter
-from qiskit.quantum_info.operators import Operator
 
 
-def N_X(nqubits: int, N: int):
+def n_x_gates(nqubits: int, N: int):
     """ Creates the circuit which applies X gates to all qubits N times.
     """
-
     circ = QuantumCircuit(nqubits, nqubits)
     for i in range(N):
         for qubit in range(nqubits):
@@ -18,4 +11,32 @@ def N_X(nqubits: int, N: int):
     return circ
 
 
-N_X(1, 1)
+def n_sx_gates(nqubits: int, N: int):
+    """ Creates the circuit which applies SX gates to all qubits N times.
+    """
+    circ = QuantumCircuit(nqubits, nqubits)
+    for i in range(N):
+        for qubit in range(nqubits):
+            circ.x(qubit)
+    return circ
+
+
+def n_h_gates(nqubits: int, N: int):
+    """ Creates the circuit which applies H gates on all qubits N times.
+    """
+    circ = QuantumCircuit(nqubits, nqubits)
+    for i in range(N):
+        for qubit in range(nqubits):
+            circ.h(qubit)
+    return circ
+
+
+def n_cnot_gates(N: int):
+    """ Creates the circuit which applies N CNOT gates on two qubits.
+    """
+    circ = QuantumCircuit(2, 2)
+    for i in range(N):
+        circ.cx(0, 1)
+    return circ
+
+
