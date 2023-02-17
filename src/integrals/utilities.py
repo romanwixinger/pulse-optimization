c899fb28dfba8557297e10bd0208a32e30304848
romanwixinger <roman.wixinger@gmail.com>
2023-02-17 10:51:01 +0100

    [workflow] Added some utilities.

---
+++ src/integrals/utilities.py
@@ -0,0 +1,18 @@
+"""
+Lookup for the integrands and other utilities.
+"""
+
+
+integrands = [
+    "sin(theta)**2",
+    "sin(theta/2)**4",
+    "sin(theta)*sin(theta/2)**2",
+    "sin(theta/2)**2",
+    "cos(theta)**2",
+    "sin(theta)*cos(theta)",
+    "sin(theta)*cos(theta)",
+    "sin(theta)",
+    "cos(theta/2)**2"
+]
+
+markers = [".", "^", "o", "2", "*", "D", "x", "X", "+"]
