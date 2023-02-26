bceb190154ba7bc3f62a56c9e68011f58e216124
romanwixinger <roman.wixinger@gmail.com>
2023-02-26 18:13:00 +0100

    [workflow] Updated integrands.

--- src/integrals/utilities.py
+++ src/integrals/utilities.py
@@ -4,15 +4,14 @@ Lookup for the integrands and other utilities.
 
 
 integrands = [
-    "sin(theta)**2",
-    "sin(theta/2)**4",
-    "sin(theta)*sin(theta/2)**2",
-    "sin(theta/2)**2",
-    "cos(theta)**2",
-    "sin(theta)*cos(theta)",
-    "sin(theta)*cos(theta)",
-    "sin(theta)",
-    "cos(theta/2)**2"
+    "sin(theta/a)**2",
+    "sin(theta/(2*a))**4",
+    "sin(theta/a)*sin(theta/(2*a))**2",
+    "sin(theta/(2*a))**2",
+    "cos(theta/a)**2",
+    "sin(theta/a)*cos(theta/a)",
+    "sin(theta/a)",
+    "cos(theta/(2*a))**2"
 ]
 
 markers = [".", "^", "o", "2", "*", "D", "x", "X", "+"]
