0ea47ce2cb7a055abe095c28fe384683f11973ca
romanwixinger <roman.wixinger@gmail.com>
2023-02-26 18:13:38 +0100

    [workflow] Minor update.

--- main/plot_pulse.py
+++ main/plot_pulse.py
@@ -8,11 +8,14 @@ from src.pulses.visualizations import plot_pulses, plot_parametrizations
 
 if __name__ == "__main__":
 
+    # Folder
+    plot_folder = "plots/pulses"
+
     # Plot pulses
     print("Plot normal pulses")
-    plot_pulses(pulse_lookup, "plots/pulses/normal_pulses.png")
-    plot_parametrizations(pulse_lookup, "plots/pulses/normal_parametrizations.png")
+    plot_pulses(pulse_lookup, "{plot_folder}/normal_pulses.png")
+    plot_parametrizations(pulse_lookup, "{plot_folder}/normal_parametrizations.png")
 
     print("Plot gaussian pulses")
-    plot_pulses(gaussian_pulse_lookup, "plots/pulses/gaussian_pulse.png", "loc = ")
-    plot_parametrizations(gaussian_pulse_lookup, "plots/pulses/gaussian_parametrizations.png", "loc = ")
+    plot_pulses(gaussian_pulse_lookup, "{plot_folder}/gaussian_pulse.png", "loc = ")
+    plot_parametrizations(gaussian_pulse_lookup, "{plot_folder}/gaussian_parametrizations.png", "loc = ")
