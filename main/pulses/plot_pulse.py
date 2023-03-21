d9fa86e4ccf5ec1478375834b281bfc0ceac9c9a
romanwixinger <roman.wixinger@gmail.com>
2023-03-21 14:15:32 +0100

    [ansatz] Added ReluPowerPulse.
    The waveform of this pulse is constrained to non-negative values. We recommend using a0 > 0 to make sure that the pulse has at least a part where it is positive. When this is not given, the pulse cannot be constructed.
    
    Note:
    - In quantum-gates we have to make the check for monotonicity less strict, as the constant parts of the parametrization can lead to slight non-monotonic behaviour on the scale 1e-9.

--- main/pulses/plot_pulse.py
+++ main/pulses/plot_pulse.py
@@ -3,7 +3,7 @@
 
 from pulse_opt.pulses.pulses import normal_pulse_lookup
 from pulse_opt.pulses.pulses import gaussian_pulse_lookup_10 as gaussian_pulse_lookup
-from pulse_opt.pulses.power_series import power_pulse_lookup
+from pulse_opt.pulses.power_series import power_pulse_lookup, relu_power_pulse_lookup
 from pulse_opt.pulses.visualizations import plot_pulses, plot_parametrizations
 
 
@@ -25,6 +25,12 @@ def main_power(plot_folder: str):
     plot_parametrizations(power_pulse_lookup, f"{plot_folder}/power_parametrizations.pdf", "f(x) = ")
 
 
+def main_relu_power(plot_folder: str):
+    print("Relu Power pulses")
+    plot_pulses(relu_power_pulse_lookup, f"{plot_folder}/power_pulse.pdf", "f(x) = ")
+    plot_parametrizations(relu_power_pulse_lookup, f"{plot_folder}/power_parametrizations.pdf", "f(x) = ")
+
+
 if __name__ == "__main__":
 
     # Folder
@@ -32,6 +38,6 @@ if __name__ == "__main__":
 
     # main_normal(plot_folder)
     # main_gaussian(plot_folder)
-    main_power(plot_folder)
-
+    # main_power(plot_folder)
+    main_relu_power(plot_folder)
 
--- pulse_opt/pulses/power_series.py
+++ pulse_opt/pulses/power_series.py
@@ -5,18 +5,22 @@ Maybe we can use this resource: https://docs.sympy.org/latest/modules/series/for
 
 
 import numpy as np
+import scipy.integrate
+import scipy.interpolate
 
 from quantum_gates.pulses import Pulse
 
 
 class PowerPulse(Pulse):
-    """
+    """Pulse based on Power series.
+
     Note:
-        Just writing out the power series and using numpy for the calculation make the code much more readable.
-
+        The coefficients should be such that integral over the interval [0,1] is positive. Otherwise, the coefficients
+        will be multiplied by a factor of -1. Moreover, the coefficients will be scaled to make the pulse well-defined
+        with a total area of 1.
     """
 
-    def __init__(self, coefficients: np.array, perform_checks: bool=True):
+    def __init__(self, coefficients: np.array, perform_checks: bool=False):
         super(PowerPulse, self).__init__(
             pulse=self._construct_pulse(coefficients),
             parametrization=self._construct_parametrization(coefficients),
@@ -57,9 +61,59 @@ class PowerPulse(Pulse):
         return parametrization
 
 
+class ReluPowerPulse(Pulse):
+    """ Rectified version of the PowerPulse, in which negative pulse values are set to zero.
+
+    Note:
+        To ensure that the pulse has a non-vanishing support with f(x)>0, we recommend setting the first
+        coefficient (a0) to a positive value.
+    """
+
+    def __init__(self, coefficients: np.array, perform_checks: bool=False):
+        pulse, parametrization = self._construct_pulse_and_parametrization(coefficients)
+        if coefficients[0] <= 0:
+            print("Warning, using non-positive first coefficient ")
+        super(ReluPowerPulse, self).__init__(
+            pulse=pulse,
+            parametrization=parametrization,
+            perform_checks=perform_checks,
+            use_lookup=False
+        )
+
+    @staticmethod
+    def _construct_pulse_and_parametrization(coefficients: np.array):
+        """ Constructs the waveform from the real coefficients. """
+
+        # Input validation
+        assert isinstance(coefficients, np.ndarray)
+        if np.sum(np.abs(coefficients)) < 1e-9:
+            return lambda x: 1, lambda x: x
+
+        # Construction
+        raw_pulse = lambda x: sum((a_i * x**i for i, a_i in enumerate(coefficients)))
+        non_zero_pulse = lambda x: max(0.0, raw_pulse(x))
+        total_integral, abserr = scipy.integrate.quad(non_zero_pulse, 0, 1)
+        assert total_integral > 0, f"Expected non-zero pulse but found total integral {total_integral}."
+
+        def pulse(x):
+            return non_zero_pulse(x) / total_integral
+
+        def parametrization(x):
+            return scipy.integrate.quad(pulse, 0, x)[0]
+
+        return pulse, parametrization
+
+
 power_pulse_lookup = {
     "power_1": PowerPulse(np.array([1])),
     "power_x": PowerPulse(np.array([0, 1])),
     "power_x_squared": PowerPulse(np.array([0, 0, 1])),
     "power_x_minus_x_squared": PowerPulse(np.array([0, 1, -1])),
+}
+
+
+relu_power_pulse_lookup = {
+    "power_1": ReluPowerPulse(np.array([1])),
+    "power_x": ReluPowerPulse(np.array([0, 1])),
+    "x_squared_minus_x": ReluPowerPulse(np.array([0.2, -1.0, 1.0])),
 }
