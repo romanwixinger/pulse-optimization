5063032235a2b5cdb51f0dc595f5d78360cfc617
romanwixinger <roman.wixinger@gmail.com>
2023-03-31 17:10:03 +0200

    [ansatz] Moved pulse instances to main and refactored PowerPulse.
    We also move the validation to the unit tests. Otherwise the PowerPulse class is to heavy.

--- main/pulses/plot_pulse.py
+++ main/pulses/plot_pulse.py
@@ -1,9 +1,11 @@
 """Visualizes the pulses.
 """
 
+import numpy as np
+
 from pulse_opt.pulses.linear_pulses import linear_pulse_lookup
 from pulse_opt.pulses.gaussian_pulses import gaussian_pulse_lookup_10 as gaussian_pulse_lookup
-from pulse_opt.pulses.power_pulses import power_pulse_lookup, relu_power_pulse_lookup, shifted_power_pulse_lookup
+from pulse_opt.pulses.power_pulses import PowerPulse, ReluPowerPulse
 from pulse_opt.pulses.visualizations import plot_pulses, plot_parametrizations
 
 
@@ -21,18 +23,35 @@ def main_gaussian(plot_folder: str):
 
 def main_power(plot_folder: str):
     print("Power pulses")
+    power_pulse_lookup = {
+        "power_1": PowerPulse(np.array([1])),
+        "power_x": PowerPulse(np.array([0, 1])),
+        "power_x_squared": PowerPulse(np.array([0, 0, 1])),
+        "power_x_minus_x_squared": PowerPulse(np.array([0, 1, -1])),
+    }
     plot_pulses(power_pulse_lookup, f"{plot_folder}/power_pulse.pdf", "f(x) = ")
     plot_parametrizations(power_pulse_lookup, f"{plot_folder}/power_parametrizations.pdf", "f(x) = ")
 
 
 def main_shifted_power(plot_folder: str):
     print("Shifted power pulses")
+    shifted_power_pulse_lookup = {
+        "shifted_power_1": PowerPulse(np.array([1]), shift=0.45),
+        "shifted_power_x": PowerPulse(np.array([0, 1]), shift=0.45),
+        "shifted_power_x_squared": PowerPulse(np.array([0, 0, 1]), shift=0.45),
+        "shifted_power_x_minus_x_squared": PowerPulse(np.array([0, 1, -1]), shift=0.45),
+    }
     plot_pulses(shifted_power_pulse_lookup, f"{plot_folder}/power_pulse.pdf", "f(x-0.5) = ")
     plot_parametrizations(shifted_power_pulse_lookup, f"{plot_folder}/shifted_power_parametrizations.pdf", "f(x) = ")
 
 
 def main_relu_power(plot_folder: str):
     print("Relu power pulses")
+    relu_power_pulse_lookup = {
+        "relu_power_1": ReluPowerPulse(np.array([1])),
+        "relu_power_x": ReluPowerPulse(np.array([0, 1])),
+        "relu_x_squared_minus_x": ReluPowerPulse(np.array([0.2, -1.0, 1.0])),
+    }
     plot_pulses(relu_power_pulse_lookup, f"{plot_folder}/power_pulse.pdf", "f(x) = ")
     plot_parametrizations(relu_power_pulse_lookup, f"{plot_folder}/relu_power_parametrizations.pdf", "f(x) = ")
 
--- pulse_opt/pulses/power_pulses.py
+++ pulse_opt/pulses/power_pulses.py
@@ -37,14 +37,12 @@ class PowerPulse(Pulse):
     """
 
     def __init__(self, coefficients: np.array, shift: float=0.0, perform_checks: bool=False):
+        # Input validation
+        PowerPulse._input_validation(coefficients, shift)
+
         # Setup
         pulse = self._construct_pulse(coefficients, shift)
         parametrization = self._construct_parametrization(coefficients, shift)
-
-        # Validation
-        pulse_integrates_to_one(pulse)
-        parametrization_has_valid_endpoints(parametrization)
-        pulse_and_parametrization_are_compatible(pulse, parametrization)
 
         # Initialize parent class
         super(PowerPulse, self).__init__(
@@ -55,35 +53,43 @@ class PowerPulse(Pulse):
         )
 
     @staticmethod
+    def _input_validation(coefficients: np.array, shift: float):
+        assert isinstance(coefficients, np.ndarray) or isinstance(coefficients, list),\
+            f"Expected coefficients to be of type np.ndarray or list but found {type(coefficients)}."
+        assert isinstance(shift, float),\
+            f"Expected shift to be of type float but found {type(shift)}."
+        assert abs(PowerPulse._compute_integral(coefficients, shift)) > 1e-9,\
+            f"Expected coefficients such that the total integral is bigger than 1e-9, but found otherwise."
+
+    @staticmethod
+    def _compute_integral(coefficients: np.array, shift: float) -> float:
+        return sum((a_i * ((1.0 - shift)**(i+1) - (0.0 - shift)**(i+1))/(i+1) for i, a_i in enumerate(coefficients)))
+
+    @staticmethod
     def _construct_pulse(coefficients: np.array, shift: float):
         """ Constructs the waveform from the real coefficients. """
-
-        # Input validation
-        assert isinstance(coefficients, np.ndarray)
-        total_integral = sum((a_i * ((1.0 - shift)**(i+1) - (0.0 - shift)**(i+1))/(i+1) for i, a_i in enumerate(coefficients)))
-        if abs(total_integral) < 1e-9:
-            return lambda x: 1
+        total_integral = PowerPulse._compute_integral(coefficients, shift)
 
         def waveform(x):
             """ Waveform f: [0,1] -> R of the pulse.
             """
             return sum((a_i * (x-shift)**i for i, a_i in enumerate(coefficients))) / total_integral
+
         return waveform
 
     @staticmethod
     def _construct_parametrization(coefficients: np.array, shift):
         """ Constructs the pulse parametrization from the real coefficients. """
 
-        # Input validation
-        assert isinstance(coefficients, np.ndarray)
-        total_integral = sum((a_i * ((1.0 - shift)**(i+1) - (0.0 - shift)**(i+1))/(i+1) for i, a_i in enumerate(coefficients)))
-        if abs(total_integral) < 1e-9:
-            return lambda x: x
+        total_integral = PowerPulse._compute_integral(coefficients, shift)
 
         def parametrization(x):
             """ Parameter integral F: [0,1] -> [0,1] of the waveform the pulse.
             """
-            return sum((a_i * ((x - shift)**(i+1) - (0.0 - shift)**(i+1))/(i+1) for i, a_i in enumerate(coefficients))) / total_integral
+            return sum(
+                (a_i * ((x - shift)**(i+1) - (0.0 - shift)**(i+1))/(i+1) for i, a_i in enumerate(coefficients))
+            ) / total_integral
+
         return parametrization
 
 
@@ -143,25 +149,3 @@ class ReluPowerPulse(Pulse):
             return scipy.integrate.quad(pulse, 0, x)[0]
 
         return pulse, parametrization
-
-
-power_pulse_lookup = {
-    "power_1": PowerPulse(np.array([1])),
-    "power_x": PowerPulse(np.array([0, 1])),
-    "power_x_squared": PowerPulse(np.array([0, 0, 1])),
-    "power_x_minus_x_squared": PowerPulse(np.array([0, 1, -1])),
-}
-
-shifted_power_pulse_lookup = {
-    "shifted_power_1": PowerPulse(np.array([1]), shift=0.45),
-    "shifted_power_x": PowerPulse(np.array([0, 1]), shift=0.45),
-    "shifted_power_x_squared": PowerPulse(np.array([0, 0, 1]), shift=0.45),
-    "shifted_power_x_minus_x_squared": PowerPulse(np.array([0, 1, -1]), shift=0.45),
-}
-
-
-relu_power_pulse_lookup = {
-    "relu_power_1": ReluPowerPulse(np.array([1])),
-    "relu_power_x": ReluPowerPulse(np.array([0, 1])),
-    "relu_x_squared_minus_x": ReluPowerPulse(np.array([0.2, -1.0, 1.0])),
-}
