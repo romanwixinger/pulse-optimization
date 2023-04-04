999b04c5196714d25a4cfc37a34d4cffc70cfe8a
romanwixinger <roman.wixinger@gmail.com>
2023-04-04 17:23:29 +0200

    [ansatz] Replaced loss functions with classes.
    Goal:
    - We want to have a class Loss(...) that can be used like: loss = Loss(...), loss_value = loss(coeff).
    - This means that the loss can be configured in the beginning and then it has the right interface to be used by scipy.optimize.minimize afterwards with a single argument (np.array).
    
    Solution:
    - We use that Python classes have the magic method __call__().
    - This method allows us to call the instance like a normal function.
    
    Todo:
    - Unit test the new functionality.

--- pulse_opt/integrals/loss_functions.py
+++ pulse_opt/integrals/loss_functions.py
@@ -2,46 +2,128 @@
 """
 
 import numpy as np
-import scipy.optimize
 
 from quantum_gates.integrators import Integrator
 
 from .utilities import integrands
-from ..pulses.power_pulses import PowerPulse, ReluPowerPulse
+from ..pulses.basis import PulseFactory, PowerFactory, FourierFactory, GaussianFactory
 
 
-def power_loss(coefficients: np.array, shift: float=0.0, theta=np.pi/2, a: float=1.0):
+class Loss(object):
+    """ Acts as a loss function.
+
+    Args:
+        factoryClass (PulseFactory): Class of the factory that creates pulses with specific coefficients.
+        factoryArgs (dict): Lookup of the extra arguments that the factoryClass needs to setup an instance.
+        weights (np.array): Weight we give each Ito integral in the loss, defaults to np.ones(8).
+        theta (float): Upper limit of the integration, total area of the pulse.
+        a (float): Scaling parameter in the Ito integrals.
+
+    Attributes:
+        factory (PulseFactory): Instance of the factoryClass setup with the factoryArgs.
+        weights (np.array): Weight we give each Ito integral in the loss, defaults to np.ones(8).
+        theta (float): Upper limit of the integration, total area of the pulse.
+        a (float): Scaling parameter in the Ito integrals.
+
+    Example:
+        .. code:: python
+
+            import numpy as np
+            from pulse_opt.integrals.loss_functions import Loss
+            from pulse_opt.pulses.basis import PowerFactory
+
+            loss = Loss(
+                factoryClass=PowerFactory,
+                factoryArgs={"shift": 0.5, "n": 3, "perform_checks": False},
+                weights=np.ones(8),
+                theta=np.pi/2,
+                a=1.0
+            )
+
+            coeff = [1.0, 0.0, 0.0, 0.0]
+
+            print(f"The sum of all Ito integrals for a PowerPulse with coefficients {coeff} is {loss(coeff)}.")
+
+    Note:
+        We design this object as a class such that the arguments can be used on instantiation time.
+    """
+
+    def __init__(self,
+                 factoryClass,
+                 factoryArgs: dict,
+                 weights: np.array=np.ones(8),
+                 theta: float=np.pi/2,
+                 a: float=1.0):
+        self.factory = factoryClass(**factoryArgs)
+        self.weights = weights
+        self.theta = theta
+        self.a = a
+
+    def __call__(self, coefficients: np.array):
+        print("Coefficients: ", coefficients)
+        pulse = self.factory.sample(coefficients)
+        integrator = Integrator(pulse=pulse)
+        return sum((integrator.integrate(integrand, theta=self.theta, a=self.a) for integrand in integrands))
+
+
+class PowerLoss(Loss):
     """ Computes the total variance of the Ito integrals for a PowerPulse.
 
     Args:
-        coefficients (np.array): Power series coefficients [a0, ..., an].
-        shift (np.array): Shift of the basis functions: x -> (x - shift).
-        theta (float): Total area of the pulse, angle by which we turn on the Bloch sphere.
-        a (float): Scaling parameter which is part of the integrals.
+        shift (float): How much the power series should be shifted.
+        n (int): Degree of the polynomial: Basis has n+1 functions.
+        weights (np.array): Weight we give each Ito integral in the loss, defaults to np.ones(8).
+        theta (float): Upper limit of the integration, total area of the pulse, defaults to np.pi/2.
+        a (float): Scaling parameter in the Ito integrals, defaults to 1.0
+
+    Example:
+        .. code:: python
+
+import numpy as np
+from pulse_opt.integrals.loss_functions import PowerLoss
+
+loss = PowerLoss(shift=0.5, n=2)
+coeff = [1.0, 0.0, 0.0]  # n+1 coefficients
+print(f"The sum of all Ito integrals for a FourierPulse with coefficients {coeff} is {loss(coeff)}.")
     """
-    pulse = PowerPulse(coefficients=coefficients, shift=shift, perform_checks=False)
-    integrator = Integrator(pulse=pulse)
-    return sum((integrator.integrate(integrand, theta=theta, a=a) for integrand in integrands))
+
+    def __init__(self, shift: float=0.5, n: int=3, weights: np.array=np.ones(8), theta: float=np.pi/2, a: float=1.0):
+        super(PowerLoss, self).__init__(
+            factoryClass=PowerFactory,
+            factoryArgs={"shift": shift, "n": n, "perform_checks": False},
+            weights=weights,
+            theta=theta,
+            a=a
+        )
 
 
-def power_relu_loss(coefficients: np.array, shift: float=0.0, theta=np.pi/2, a: float=1.0):
-    """ Computes the total variance of the Ito integrals for a ReluPowerPulse.
+class FourierLoss(Loss):
+    """ Computes the total variance of the Ito integrals for a FourierPulse.
 
     Args:
-        coefficients (np.array): Power series coefficients [a0, ..., an].
-        shift (np.array): Shift of the basis functions: x -> (x - shift).
-        theta (float): Total area of the pulse, angle by which we turn on the Bloch sphere.
-        a (float): Scaling parameter which is part of the integrals.
+        shift (float): How much the power series should be shifted.
+        n (int): Maximum number of zero-crossing in the basis functions: Basis has 2(n+1) functions.
+        weights (np.array): Weight we give each Ito integral in the loss, defaults to np.ones(8).
+        theta (float): Upper limit of the integration, total area of the pulse, defaults to np.pi/2.
+        a (float): Scaling parameter in the Ito integrals, defaults to 1.0
+
+
+    Example:
+        .. code:: python
+
+            import numpy as np
+            from pulse_opt.integrals.loss_functions import FourierLoss
+
+            loss = FourierLoss(shift=0.5, n=1)
+            coeff = [1.0, 0.0, 0.0, 0.0]  # 2(n+1) coefficients
+            print(f"The sum of all Ito integrals for a FourierPulse with coefficients {coeff} is {loss(coeff)}.")
     """
-    pulse = ReluPowerPulse(coefficients=coefficients, shift=shift, perform_checks=False)
-    integrator = Integrator(pulse=pulse)
-    return sum((integrator.integrate(integrand, theta=theta, a=a) for integrand in integrands))
 
-
-power_relu_theta_pi_half = lambda coeff: power_relu_loss(coefficients=coeff, shift=0.5, theta=np.pi/2)
-power_relu_theta_pi = lambda coeff: power_relu_loss(coefficients=coeff, shift=0.5, theta=np.pi)
-
-loss_lookup = {
-    "power_relu_theta_pi_half": power_relu_theta_pi_half,
-    "power_relu_theta_pi": power_relu_theta_pi,
-}
+    def __init__(self, shift: float=0.5, n: int=3, weights: np.array=np.ones(8), theta: float=np.pi/2, a: float=1.0):
+        super(FourierLoss, self).__init__(
+            factoryClass=FourierFactory,
+            factoryArgs={"shift": shift, "n": n, "perform_checks": False},
+            weights=weights,
+            theta=theta,
+            a=a
+        )
