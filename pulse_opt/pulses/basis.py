e22c0761ce7a12c7410780343243f0dc035a26c5
romanwixinger <roman.wixinger@gmail.com>
2023-04-04 08:10:12 +0200

    [ansatz] Refactored FourierFactory and PowerFactory.
    Problem:
    - To test the functions and integrals it was necessary to setup a basis.
    - The constructor was clustered with code.
    
    Solution:
    - Separate the generator into functions get_functions() and get_integrals().

--- pulse_opt/pulses/basis.py
+++ pulse_opt/pulses/basis.py
@@ -189,6 +189,31 @@ class PowerFactory(PulseFactory):
             ),
             perform_checks=perform_checks)
 
+    @staticmethod
+    def get_functions(n: int) -> list[callable]:
+        """ Generates a list of sin and cos functions that form the basis.
+
+        Args:
+            n (int): Degree of the polynomial to be used.
+
+        Returns:
+            Basis functions, polynomials up to degree n.
+        """
+
+        return [(lambda x, power=i: x ** power) for i in range(n+1)]
+
+    @staticmethod
+    def get_integrals(n: int) -> list[callable]:
+        """Generates a list of antiderivatives of the functions returned by the get_functions() method.
+
+        Args:
+            n (int): Degree of the polynomial to be used.
+
+        Returns:
+            Integrals of the basis functions.
+        """
+        return [(lambda x, power=i: (x ** (power + 1))/(power + 1)) for i in range(n+1)]
+
 
 class FourierFactory(PulseFactory):
     """Constructs pulses based on the fourier series, namely sin (odd) and cos (even) functions.
@@ -203,15 +228,43 @@ class FourierFactory(PulseFactory):
         self.n = n
         super(FourierFactory, self).__init__(
             basis=Basis(
-                functions=[(
-                    lambda x, j=i: np.cos(x * (j//2+1) * (np.pi/2)) * (np.pi/2) if j % 2 == 0
-                    else np.sin(x * (j//2+1) * (np.pi/2)) * (np.pi/2)
-                ) for i in range(2*(self.n+1))],
-                integrals=[(
-                    lambda x, j=i: np.sin(x * (j//2+1) * (np.pi/2)) - 0.0 if j % 2 == 0
-                    else -np.cos(x * (j//2+1) * np.pi/2) + 1.0
-                ) for i in range(2*(self.n+1))],
+                functions=FourierFactory.get_functions(n),
+                integrals=FourierFactory.get_integrals(n),
                 shift=self.shift,
                 bounds=[(1e-3, None), (1e-3, None)] + [(None, None) for i in range(2 * self.n)]
             ),
             perform_checks=perform_checks)
+
+    @staticmethod
+    def get_functions(n: int) -> list[callable]:
+        """ Generates a list of sin and cos functions that form the basis.
+
+        Args:
+            n (int): Maximum number of zero crossing in the basis functions.
+
+        Returns:
+            Basis functions based on sin() and cos() with increasing number of zero crossings. The first few functions
+                are up to constants of the form
+                [c*cos(c*x), c*sin(c*x), c*cos(2*c*x), c*sin(2*c*x), c*cos(3*c*x),...]
+                In this case, c = pi/2 such that the first function pair crosses zero at the boundary, and the i-th
+                function pair has i zero crossing, where i is an index (starts at 0).
+        """
+        return [(
+            lambda x, j=i: np.cos(x * (j//2+1) * (np.pi/2)) * (np.pi/2) if j % 2 == 0
+            else np.sin(x * (j//2+1) * (np.pi/2)) * (np.pi/2)
+        ) for i in range(2*(n+1))]
+
+    @staticmethod
+    def get_integrals(n: int) -> list[callable]:
+        """Generates a list of antiderivatives of the functions returned by the get_functions() method.
+
+        Args:
+            n (int): Maximum number of zero crossing in the basis functions.
+
+        Returns:
+            Integrals of the basis functions.
+        """
+        return [(
+            lambda x, j=i: np.sin(x * (j//2+1) * (np.pi/2)) - 0.0 if j % 2 == 0
+            else -np.cos(x * (j//2+1) * np.pi/2) + 1.0
+        ) for i in range(2*(n+1))]
