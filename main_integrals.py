7544404cac02a7895623600a56104ec1c4aa4828
romanwixinger <roman.wixinger@gmail.com>
2023-04-10 17:36:06 +0200

    [ansatz] Added execution file.

---
+++ main_integrals.py
@@ -0,0 +1,26 @@
+import logging
+
+from main.integrals.minimize_integrals import main
+
+logger = logging.getLogger()
+
+
+if __name__ == "__main__":
+
+    runs = [
+        'power_test',
+        'fourier_test',
+        'gaussian_test',
+        'power_small',
+        'fourier_small',
+        'gaussian_small',
+        'power',
+        'fourier',
+        'gaussian'
+    ]
+    all_results = [None for run in runs]
+
+    for i, run in enumerate(runs):
+        logger.info(f"Start run with {run} configuration.")
+        results = main(run=run, use_multiprocessing=False)
+        all_results[i] = results
