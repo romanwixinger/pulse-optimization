5d2e80863a35a801155263e7e15f1747cb3700d5
romanwixinger <roman.wixinger@gmail.com>
2023-04-10 16:30:38 +0200

    [ansatz] Deleted redundant script.

--- main/integrals/simulate_integrals.py
+++
@@ -1,9 +0,0 @@
-"""Optimization of pulses to minimize a loss function based on the integrals.
-
-We calculate the integration results for parametrized pulses and optimize the parameters to
-minimize a loss function which is a proxy for the amount of noise.
-"""
-
-
-if __name__ == '__main__':
-    pass
