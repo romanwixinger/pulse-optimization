11194e6e342d9f6f4e5a5dd1687d17128dd26a49
romanwixinger <roman.wixinger@gmail.com>
2023-04-20 09:04:14 +0200

    [cleanup] Drafted unit tests.

---
+++ tests/integrals/test_integral_utilities.py
@@ -0,0 +1,24 @@
+import pytest
+
+from pulse_opt.integrals.utilities import (
+    create_table,
+    setup_logging,
+    run_with_multiprocessing,
+    run_without_multiprocessing,
+)
+
+
+def test_create_table():
+    pass
+
+
+def test_setup_logging():
+    pass
+
+
+def test_run_with_multiprocessing():
+    pass
+
+
+def test_run_without_multiprocessing():
+    pass
