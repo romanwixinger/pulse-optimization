2feda425866a439816c1105871a57736d65570ad
romanwixinger <roman.wixinger@gmail.com>
2023-04-27 16:11:38 +0200

    [experiments] Added weights for the integrands.
    These weights describe how often the integrands come up in the Ito integrands. This helps us plot them and create appropriate losses.

--- pulse_opt/integrands/utilities.py
+++ pulse_opt/integrands/utilities.py
@@ -3,6 +3,10 @@
 Attributes:
     integrands (list): List of integrands used in the Ito integrals of the quantum-gates library.
     equal_weight_lookup (dict): Weight lookup which assigns weight 1.0 to each integrand.
+    variance_weight_lookup (dict): Assigns 1.0 to each integrand coming up as variance, rest 0.0.
+    covariance_weight_lookup (dict): Assigns 1.0 to each integrand coming up as covariance, rest 0.0.
+    deterministic_weight_lookup (dict): Assigns 1.0 to each integrand coming up in the
+        deterministic part, rest 0.0.
 """
 
 import numpy as np
@@ -21,3 +25,33 @@ integrand_lookup = {
 integrands = list(integrand_lookup.keys())
 
 equal_weight_lookup = {key: 1.0 for key in integrand_lookup.keys()}
+deterministic_weight_lookup = {
+    "sin(theta/a)**2": 1.0,
+    "sin(theta/(2*a))**4": 0.0,
+    "sin(theta/a)*sin(theta/(2*a))**2": 0.0,
+    "sin(theta/(2*a))**2": 0.0,
+    "cos(theta/a)**2": 0.0,
+    "sin(theta/a)*cos(theta/a)": 0.0,
+    "sin(theta/a)": 1.0,
+    "cos(theta/(2*a))**2": 1.0,
+}
+variance_weight_lookup = {
+    "sin(theta/a)**2": 1.0,
+    "sin(theta/(2*a))**4": 1.0,
+    "sin(theta/a)*sin(theta/(2*a))**2": 0.0,
+    "sin(theta/(2*a))**2": 0.0,
+    "cos(theta/a)**2": 1.0,
+    "sin(theta/a)*cos(theta/a)": 0.0,
+    "sin(theta/a)": 0.0,
+    "cos(theta/(2*a))**2": 0.0,
+}
+covariance_weight_lookup = {
+    "sin(theta/a)**2": 0.0,
+    "sin(theta/(2*a))**4": 0.0,
+    "sin(theta/a)*sin(theta/(2*a))**2": 1.0,
+    "sin(theta/(2*a))**2": 1.0,
+    "cos(theta/a)**2": 0.0,
+    "sin(theta/a)*cos(theta/a)": 1.0,
+    "sin(theta/a)": 1.0,
+    "cos(theta/(2*a))**2": 0.0,
+}
