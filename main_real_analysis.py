0a989012e85075969f3192cab22326eca4153c79
Roman Wixinger <roman.wixinger@gmail.com>
2023-11-26 19:13:51 +0100

    [main] Visualized the result of the real hardware run in a more intuitive way.

--- main_real_analysis.py
+++ main_real_analysis.py
@@ -5,11 +5,9 @@ import matplotlib.pyplot as plt
 # Settings
 folder = "data/real/fourier_extended_constrained"
 
-
 # Function to load data
 def load_data(filename):
     return np.loadtxt(filename)
-
 
 # Circuit lengths used in the experiment
 circuit_lengths = range(0, 110, 10)  # From 0 to 100 with step 10
@@ -17,22 +15,29 @@ circuit_lengths = range(0, 110, 10)  # From 0 to 100 with step 10
 # Scaling factors used in the experiment
 scaling_factors = np.arange(0.0, 1.0, step=0.1)
 
-# Create a plot for each scaling factor
-for scaling in scaling_factors:
-    print("Load scaling", scaling)
-    r00_data = load_data(f'{folder}/r00_X_DEVICE_1_{scaling}.txt')
-    r11_data = load_data(f'{folder}/r11_X_DEVICE_1_{scaling}.txt')
+# Initialize a figure for plotting
+plt.figure()
 
-    # Plotting the Hellinger distance for each circuit length
-    plt.plot(circuit_lengths, r00_data, label=f'Scaling {scaling:.1f} (r00)')
-    plt.plot(circuit_lengths, r11_data, label=f'Scaling {scaling:.1f} (r11)', linestyle='--')
+# Create a plot for each circuit length
+for length in circuit_lengths:
+    r00_data = []
+    r11_data = []
+    for scaling in scaling_factors:
+        print(f"Load scaling {scaling} for circuit length {length}")
+        r00_file = f'{folder}/r00_X_DEVICE_1_{scaling}.txt'
+        r11_file = f'{folder}/r11_X_DEVICE_1_{scaling}.txt'
+        r00_data.append(load_data(r00_file)[length // 10])  # Assuming data is ordered by circuit length
+        r11_data.append(load_data(r11_file)[length // 10])
 
-plt.xlabel('Circuit Length')
+    # Plotting the Hellinger distance for each scaling factor
+    plt.plot(scaling_factors, r00_data, label=f'Circuit Length {length} (r00)')
+    plt.plot(scaling_factors, r11_data, label=f'Circuit Length {length} (r11)', linestyle='--')
+
+plt.xlabel('Scaling Factor')
 plt.ylabel('Hellinger Distance')
-plt.title('Hellinger Distance vs Circuit Length for Different Scaling Factors')
+plt.title('Hellinger Distance vs Scaling Factor for Different Circuit Lengths')
 plt.legend()
 plt.grid(True)
-plt.savefig("main_real_visualization.svg")
-
-
+plt.savefig("main_real_visualization.svg")  # Saving the plot with a new filename
+plt.show()
 
