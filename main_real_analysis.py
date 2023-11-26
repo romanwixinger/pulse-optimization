27039b2cd2a8ac0f7c3b3fe4dc53a1a8b27646fd
Roman Wixinger <roman.wixinger@gmail.com>
2023-11-26 19:19:51 +0100

    [main] Separated analysis of different matrix elements to make the figure easier to understand.

--- main_real_analysis.py
+++ main_real_analysis.py
@@ -15,29 +15,29 @@ circuit_lengths = range(0, 110, 10)  # From 0 to 100 with step 10
 # Scaling factors used in the experiment
 scaling_factors = np.arange(0.0, 1.0, step=0.1)
 
-# Initialize a figure for plotting
-plt.figure()
-
-# Create a plot for each circuit length
+# Plot for r00 data
+plt.figure(figsize=(10, 6))
 for length in circuit_lengths:
-    r00_data = []
-    r11_data = []
-    for scaling in scaling_factors:
-        print(f"Load scaling {scaling} for circuit length {length}")
-        r00_file = f'{folder}/r00_X_DEVICE_1_{scaling}.txt'
-        r11_file = f'{folder}/r11_X_DEVICE_1_{scaling}.txt'
-        r00_data.append(load_data(r00_file)[length // 10])  # Assuming data is ordered by circuit length
-        r11_data.append(load_data(r11_file)[length // 10])
-
-    # Plotting the Hellinger distance for each scaling factor
-    plt.plot(scaling_factors, r00_data, label=f'Circuit Length {length} (r00)')
-    plt.plot(scaling_factors, r11_data, label=f'Circuit Length {length} (r11)', linestyle='--')
-
+    r00_data = [load_data(f'{folder}/r00_X_DEVICE_1_{scaling}.txt')[length // 10] for scaling in scaling_factors]
+    plt.plot(scaling_factors, r00_data, label=f'Length {length}')
+plt.title('r00 Data')
 plt.xlabel('Scaling Factor')
 plt.ylabel('Hellinger Distance')
-plt.title('Hellinger Distance vs Scaling Factor for Different Circuit Lengths')
-plt.legend()
+plt.legend(fontsize='small', loc='upper right')
 plt.grid(True)
-plt.savefig("main_real_visualization.svg")  # Saving the plot with a new filename
+plt.savefig("r00_data_visualization.svg")
+plt.show()
+
+# Plot for r11 data
+plt.figure(figsize=(10, 6))
+for length in circuit_lengths:
+    r11_data = [load_data(f'{folder}/r11_X_DEVICE_1_{scaling}.txt')[length // 10] for scaling in scaling_factors]
+    plt.plot(scaling_factors, r11_data, label=f'Length {length}')
+plt.title('r11 Data')
+plt.xlabel('Scaling Factor')
+plt.ylabel('Hellinger Distance')
+plt.legend(fontsize='small', loc='upper right')
+plt.grid(True)
+plt.savefig("r11_data_visualization.svg")
 plt.show()
 
