2cc13b131b559d347958d3cc5b42a8f21a95860c
Roman Wixinger <roman.wixinger@gmail.com>
2024-03-01 17:53:44 +0100

    [main] Updated the many X gates algorithm. This way it can be used in the real hardware runs.

--- pulse_opt/algorithms/algorithms.py
+++ pulse_opt/algorithms/algorithms.py
@@ -1,12 +1,17 @@
+""" Commonly used quantum circuits.
+"""
+
 from qiskit import QuantumCircuit
 
 
-def n_x_gates(nqubits: int, N: int) -> QuantumCircuit:
+def n_x_gates(nqubits: int, N: int, add_barrier=False, add_measurement=False) -> QuantumCircuit:
     """Creates the circuit which applies X gates to all qubits N times.
 
     Args:
         nqubits (int): Number of qubits on which X gates should be applied.
         N (int): Number of X gate applied on each qubit.
+        add_barrier (bool): Should the circuit have an optimization barrier after each gate?
+        add_measurement (bool): Should the circuit end with a measurement?
 
     Returns:
         Quantum circuit representing this algorithm.
@@ -15,6 +20,10 @@ def n_x_gates(nqubits: int, N: int) -> QuantumCircuit:
     for i in range(N):
         for qubit in range(nqubits):
             circ.x(qubit)
+            if add_barrier:
+                circ.barrier(range(nqubits))
+    if add_measurement:
+        circ.measure(range(nqubits), range(nqubits))
     return circ
 
 
@@ -65,5 +74,3 @@ def n_cnot_gates(N: int) -> QuantumCircuit:
     for i in range(N):
         circ.cx(0, 1)
     return circ
-
-
